37f58d7f76d988f5be36a345650047ffda0d4b00
mazharaa <mazharaa32@gmail.com>
2020-11-21 15:40:26 +0700

    Add input for DB

--- bash.sh
+++ bash.sh
@@ -2,4 +2,5 @@
 
 read -p 'Mysql Username: ' usr &&
 read -p 'Mysql Password: ' pw &&
-spark-submit --jars /get-data/mysql-connector-java-5.1.49-bin.jar /get-data/script.py $usr $pw
+read -p 'DB Target: ' db &&
+spark-submit --jars /get-data/mysql-connector-java-5.1.49-bin.jar /get-data/script.py $usr $pw $db
